42e261a850e7b125ce213758a2cd032d8a813b31
Minhee Lee <minigimbob@gmail.com>
2024-05-27 01:37:35 +0900

    ss. code refactoring

--- dataset_cleanup.py
+++ dataset_cleanup.py
@@ -10,10 +10,18 @@ ALMOST_EQUAL_THRESHOLD = 95
 TOKEN_SORT_THRESHOLD = 95
 
 
+class CsvColumnNames:
+  def __init__(self, date, title, artist):
+    self.date = date
+    self.title = title
+    self.artist = artist
+
+column_names = CsvColumnNames('Year', 'Song', 'Artist')
+
 class Info:
     def __init__(self, row):
-        self.song_title = row['Song'].lower()
-        self.artist = row['Artist'].lower()
+        self.song_title = row[column_names.title].lower()
+        self.artist = row[column_names.artist].lower()
         self.original_video_title = row['video_title'].lower()
         self.original_video_channel = row['video_channel'].lower()
 
@@ -90,7 +98,6 @@ class DatasetCleaner:
 
 
 def get_rows_to_remove(original_csv_path, things_to_remove_csv_path):
-    original_csv_path = CSV_DIR / Path('billboard_hot100_chosen.csv')
     df = pd.read_csv(original_csv_path)
     cleaner = DatasetCleaner()
 
@@ -117,16 +124,18 @@ def move_audio_files(from_dir, to_dir, original_csv_path, things_to_remove_csv_p
     original_df = pd.read_csv(original_csv_path)
     remove_df = pd.read_csv(things_to_remove_csv_path)
     for _, row in tqdm(remove_df.iterrows(), total=len(remove_df)):
-        date = row['Date']
-        song = row['Song']
-        artist = row['Artist']
+        date = row[column_names.date]
+        song = row[column_names.title]
+        artist = row[column_names.artist]
         
         song_id = get_song_id(date, song, artist)
         audio_path = from_dir / Path(f'{song_id}.mp3')
         if audio_path.exists():
             audio_path.rename(to_dir / Path(f'{song_id}.mp3'))
 
-        original_df.drop(original_df[(original_df['Date'] == date) & (original_df['Song'] == song) & (original_df['Artist'] == artist)].index, inplace=True)
+        original_df.drop(original_df[(original_df[column_names.date] == date) \
+                                     & (original_df[column_names.title] == song) \
+                                        & (original_df[column_names.artist] == artist)].index, inplace=True)
     original_df.to_csv(original_csv_path, index=False)
 
     files_in_from_dir = list(from_dir.glob('*.mp3'))
@@ -136,17 +145,10 @@ def move_audio_files(from_dir, to_dir, original_csv_path, things_to_remove_csv_p
 
 
 if __name__ == '__main__':
-    from_dir = Path('data')
-    to_dir = Path('legacy/data')
-    original_csv_path = CSV_DIR / Path('billboard_hot100_chosen.csv')
-    things_to_remove_csv_path = CSV_DIR / Path('billboard_hot100_things_to_remove.csv')
+    from_dir = Path('audio')
+    to_dir = Path('audio_tmp')
+    original_csv_path = CSV_DIR / Path('kpop_chosen.csv')
+    things_to_remove_csv_path = CSV_DIR / Path('kpop_things_to_remove.csv')
 
     get_rows_to_remove(original_csv_path, things_to_remove_csv_path)
     move_audio_files(from_dir, to_dir, original_csv_path, things_to_remove_csv_path)
-
-"""
-1/6/1968,A Working Man's Prayer,Arthur Prysock,2,False,True,True,True,my special prayer,arthur prysock - topic,https://www.youtube.com/watch?v=K5ZluuY5hK8
-7/31/1971,It's About Time,The Dillards,2,False,True,True,True,in our time,the dillards - topic,https://www.youtube.com/watch?v=tYzvoLnAJ5M
-12/11/1971,Love Potion Number Nine,The Coasters,1,False,True,True,True,love potion #9,the coasters - topic,https://www.youtube.com/watch?v=Z--L9meVdnk
-11/4/1972,I Got A Thing About You Baby,Billy Lee Riley,7,False,True,True,True,"i want you, baby",billy lee riley - topic,https://www.youtube.com/watch?v=DehdaHN1Atg
-"""
