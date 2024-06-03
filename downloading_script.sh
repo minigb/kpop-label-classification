cb6fd1071a0545f4160a8961abab880d5f599e2b
Minhee Lee <minigimbob@gmail.com>
2024-06-04 00:21:53 +0900

    update info

--- downloading_script.sh
+++ downloading_script.sh
@@ -7,6 +7,6 @@ python musicbrainz_song.py
 python musicbrainz_check_track_artist.py
 
 # Step 3: Run crawling_data.py with specified arguments
-python crawling_data.py --input_csv kpop-dataset/song_list.csv --save_csv_name kpop-dataset/csv/kpop --save_audio_dir audio
+# python crawling_data.py --input_csv kpop-dataset/song_list.csv --save_csv_name kpop-dataset/csv/kpop --save_audio_dir audio
 
 echo "All scripts have been executed successfully."
