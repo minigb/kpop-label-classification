a153739ae40ebab51c1b4eaea6ad81f38ecbbf35
Minhee Lee <minigimbob@gmail.com>
2024-06-06 02:59:09 +0900

    Create config for column_name

--- make_label_column.py
+++ make_label_column.py
@@ -5,17 +5,10 @@ from tqdm import tqdm
 from collections import Counter
 
 class ArtistAndLabelMatcher:
-    # Column names
-    # TODO(minigb): 1) Change column names to singular form 2) control this by config
-    # Need to also fix the other codes.
-    ARTIST = 'artists'
-    LABEL = 'labels'
-    START = 'start_date'
-    END = 'end_date'
-
     def __init__(self, config):
         self.artists_per_label_dir = Path(config.data.artists_dir)
         self.save_fn = Path(config.kpop_dataset.artist_list_csv_fn)
+        self.column_name = config.column_name
 
     def run(self):
         artist_label_list = self._get_artist_labels()
@@ -28,22 +21,22 @@ class ArtistAndLabelMatcher:
         for csv_file in tqdm(list(self.artists_per_label_dir.glob('*.csv'))):
             label_name = csv_file.stem
             df = pd.read_csv(csv_file)
-            for artist in df[self.ARTIST]:
-                artist_label.append({self.ARTIST: artist, self.LABEL: label_name})
+            for artist in df[self.column_name.artist]:
+                artist_label.append({self.column_name.artist: artist, self.column_name.label: label_name})
 
         return artist_label
 
     def _get_df_with_year_added(self, artist_label_list):
-        counter = Counter([x[self.ARTIST] for x in artist_label_list])
+        counter = Counter([x[self.column_name.artist] for x in artist_label_list])
         for artist_and_label in artist_label_list:
-            artist_name = artist_and_label[self.ARTIST]
-            artist_and_label[self.START] = artist_and_label[self.END] = 0 if counter[artist_name] > 1 else None
+            artist_name = artist_and_label[self.column_name.artist]
+            artist_and_label[self.column_name.label_start] = artist_and_label[self.column_name.label_end] = 0 if counter[artist_name] > 1 else None
 
         df = pd.DataFrame(artist_label_list)
-        df[self.START] = df[self.START].astype("Int32")
-        df[self.END] = df[self.END].astype("Int32")
+        df[self.column_name.label_start] = df[self.column_name.label_start].astype("Int32")
+        df[self.column_name.label_end] = df[self.column_name.label_end].astype("Int32")
         
-        df = df.sort_values(by=[self.ARTIST, self.LABEL])
+        df = df.sort_values(by=[self.column_name.artist, self.column_name.label])
         return df
 
     def _merge_original_and_updated_df(self, new_df):
@@ -55,7 +48,7 @@ class ArtistAndLabelMatcher:
         idx_to_remove = []
         for idx, row in original_df.iterrows():
             # if empty
-            if new_df[(new_df[self.ARTIST] == row[self.ARTIST]) & (new_df[self.LABEL] == row[self.LABEL])].empty:
+            if new_df[(new_df[self.column_name.artist] == row[self.column_name.artist]) & (new_df[self.column_name.label] == row[self.column_name.label])].empty:
                 idx_to_remove.append(idx)
         original_df = original_df.drop(idx_to_remove)
 
@@ -63,7 +56,7 @@ class ArtistAndLabelMatcher:
         idx_to_remove = []
         for idx, row in new_df.iterrows():
             # if not empty
-            if not original_df[(original_df[self.ARTIST] == row[self.ARTIST]) & (original_df[self.LABEL] == row[self.LABEL])].empty:
+            if not original_df[(original_df[self.column_name.artist] == row[self.column_name.artist]) & (original_df[self.column_name.label] == row[self.column_name.label])].empty:
                 idx_to_remove.append(idx)
         new_df = new_df.drop(idx_to_remove)
 
@@ -74,7 +67,7 @@ class ArtistAndLabelMatcher:
     def _save_result(self, df):
         self.save_fn.parent.mkdir(parents=True, exist_ok=True)
         
-        df = df.sort_values(by=[self.ARTIST, self.LABEL])
+        df = df.sort_values(by=[self.column_name.artist, self.column_name.label])
         df.to_csv(self.save_fn, index=False)
 
 
