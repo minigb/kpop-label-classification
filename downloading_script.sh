6fc7554eaf7283a2dc1c96b51846bc779529da4f
Minhee Lee <minigimbob@gmail.com>
2024-06-06 01:22:51 +0900

    ss

--- downloading_script.sh
+++ downloading_script.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-python musicbrainz_get_artist_of_label.py
-# python musicbrainz_song.py
+# python musicbrainz_get_artist_of_label.py
+python musicbrainz_song.py
 # python musicbrainz_check_track_artist.py
 # python crawling_data.py
 
