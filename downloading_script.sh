b3a0c085af251ca5651d428dd9cac3a0a7b54d8f
Minhee Lee <minigimbob@gmail.com>
2024-06-06 01:38:51 +0900

    done!

--- downloading_script.sh
+++ downloading_script.sh
@@ -4,7 +4,7 @@ set -e
 
 # python musicbrainz_get_artist_of_label.py
 python musicbrainz_song.py
-# python musicbrainz_check_track_artist.py
+python musicbrainz_check_track_artist.py
 # python crawling_data.py
 
 echo "All scripts have been executed successfully."
