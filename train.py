7f2c88cc63e4c16ef75e1ceb4bba07b204ef110e
Minhee Lee <minigimbob@gmail.com>
2024-06-08 23:51:22 +0900

    snapshot ㅠㅠ

--- train.py
+++ train.py
@@ -1,36 +1,36 @@
 import torch
 import torch.optim as optim
 import torch.nn as nn
-from torch.utils.data import DataLoader, TensorDataset
+from torch.utils.data import DataLoader
 import hydra
 from omegaconf import DictConfig
 
-from model_zoo import AudioModel
-from trainer import Trainer
+import model_zoo
+from dataset import KpopDataset
+from trainer import train_model
 
 @hydra.main(config_path='config', config_name='packed')
 def main(cfg: DictConfig):
-    device = cfg.train.device
+    device = cfg.model.cfg.device
 
-    # Dummy dataset for demonstration purposes
     num_samples = 1000
     inputs = torch.randn(num_samples, cfg.model.n_in_channel, cfg.model.sr * 30)  # Example input tensor
     labels = torch.randint(0, cfg.model.n_label_class, (num_samples,))  # Example label tensor
     years = torch.randint(0, cfg.model.n_year_class, (num_samples,))  # Example year tensor
 
-    dataset = TensorDataset(inputs, labels, years)
+    dataset = KpopDataset(inputs, labels, years)
     train_size = int(0.8 * len(dataset))
     val_size = len(dataset) - train_size
     train_dataset, val_dataset = torch.utils.data.random_split(dataset, [train_size, val_size])
     train_loader = DataLoader(train_dataset, batch_size=cfg.batch_size, shuffle=True)
     val_loader = DataLoader(val_dataset, batch_size=cfg.batch_size, shuffle=False)
 
-    model = AudioModel(cfg.model).to(device)
+    model_class = getattr(model_zoo, cfg.model.name)
+    model = model_class(cfg.model).to(device)
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=cfg.lr)
 
-    trainer = Trainer(model, train_loader, val_loader, criterion, optimizer, cfg.num_epochs, device)
-    trainer.train()
+    train_model(model, train_loader, val_loader, criterion, optimizer, cfg.num_epochs, device)
 
 if __name__ == '__main__':
     main()
--- trainer.py
+++ trainer.py
@@ -3,65 +3,55 @@ import torch.optim as optim
 import torch.nn as nn
 from torch.utils.data import DataLoader
 
-class Trainer:
-    def __init__(self, model, train_loader, val_loader, criterion, optimizer, num_epochs, device):
-        self.model = model
-        self.train_loader = train_loader
-        self.val_loader = val_loader
-        self.criterion = criterion
-        self.optimizer = optimizer
-        self.num_epochs = num_epochs
-        self.device = device
-        self.best_loss = float('inf')
+def train_one_epoch(model, dataloader, criterion, optimizer, device):
+    model.train()
+    running_loss = 0.0
+    for batch in dataloader:
+        inputs, labels, years = batch
+        inputs, labels, years = inputs.to(device), labels.to(device), years.to(device)
 
-    def train_one_epoch(self):
-        self.model.train()
-        running_loss = 0.0
-        for batch in self.train_loader:
+        optimizer.zero_grad()
+        label_output, year_output = model(inputs)
+
+        label_loss = criterion(label_output, labels)
+        year_loss = criterion(year_output, years)
+        loss = label_loss + year_loss
+
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+
+    return running_loss / len(dataloader)
+
+def validate_one_epoch(model, dataloader, criterion, device):
+    model.eval()
+    running_loss = 0.0
+    with torch.no_grad():
+        for batch in dataloader:
             inputs, labels, years = batch
-            inputs, labels, years = inputs.to(self.device), labels.to(self.device), years.to(self.device)
+            inputs, labels, years = inputs.to(device), labels.to(device), years.to(device)
 
-            self.optimizer.zero_grad()
-            label_output, year_output = self.model(inputs)
+            label_output, year_output = model(inputs)
 
-            label_loss = self.criterion(label_output, labels)
-            year_loss = self.criterion(year_output, years)
+            label_loss = criterion(label_output, labels)
+            year_loss = criterion(year_output, years)
             loss = label_loss + year_loss
-
-            loss.backward()
-            self.optimizer.step()
 
             running_loss += loss.item()
 
-        return running_loss / len(self.train_loader)
+    return running_loss / len(dataloader)
 
-    def validate_one_epoch(self):
-        self.model.eval()
-        running_loss = 0.0
-        with torch.no_grad():
-            for batch in self.val_loader:
-                inputs, labels, years = batch
-                inputs, labels, years = inputs.to(self.device), labels.to(self.device), years.to(self.device)
+def train_model(model, train_loader, val_loader, criterion, optimizer, num_epochs, device):
+    best_loss = float('inf')
+    for epoch in range(num_epochs):
+        train_loss = train_one_epoch(model, train_loader, criterion, optimizer, device)
+        val_loss = validate_one_epoch(model, val_loader, criterion, device)
 
-                label_output, year_output = self.model(inputs)
+        print(f'Epoch {epoch+1}/{num_epochs}, Train Loss: {train_loss:.4f}, Val Loss: {val_loss:.4f}')
 
-                label_loss = self.criterion(label_output, labels)
-                year_loss = self.criterion(year_output, years)
-                loss = label_loss + year_loss
+        if val_loss < best_loss:
+            best_loss = val_loss
+            torch.save(model.state_dict(), 'best_model.pth')
 
-                running_loss += loss.item()
-
-        return running_loss / len(self.val_loader)
-
-    def train(self):
-        for epoch in range(self.num_epochs):
-            train_loss = self.train_one_epoch()
-            val_loss = self.validate_one_epoch()
-
-            print(f'Epoch {epoch+1}/{self.num_epochs}, Train Loss: {train_loss:.4f}, Val Loss: {val_loss:.4f}')
-
-            if val_loss < self.best_loss:
-                self.best_loss = val_loss
-                torch.save(self.model.state_dict(), 'best_model.pth')
-
-        print('Training complete. Best validation loss:', self.best_loss)
+    print('Training complete. Best validation loss:', best_loss)
