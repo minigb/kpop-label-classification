783ec503e26c590d773ba55e57b44dc166b84092
Minhee Lee <minigimbob@gmail.com>
2024-06-08 23:58:02 +0900

    snapshot

--- train.py
+++ train.py
@@ -18,17 +18,17 @@ def main(cfg: DictConfig):
     labels = torch.randint(0, cfg.model.n_label_class, (num_samples,))  # Example label tensor
     years = torch.randint(0, cfg.model.n_year_class, (num_samples,))  # Example year tensor
 
-    dataset = KpopDataset(inputs, labels, years)
-    train_size = int(0.8 * len(dataset))
-    val_size = len(dataset) - train_size
-    train_dataset, val_dataset = torch.utils.data.random_split(dataset, [train_size, val_size])
-    train_loader = DataLoader(train_dataset, batch_size=cfg.batch_size, shuffle=True)
-    val_loader = DataLoader(val_dataset, batch_size=cfg.batch_size, shuffle=False)
+    train_dataset = KpopDataset(cfg, cfg.dict_key.train)
+    val_dataset = KpopDataset(cfg, cfg.dict_key.valid)
+    # test_dataset = KpopDataset(cfg, cfg.dict_key.test)
+
+    train_loader = DataLoader(train_dataset, batch_size=cfg.train.batch_size, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=cfg.train.batch_size, shuffle=False)
 
     model_class = getattr(model_zoo, cfg.model.name)
     model = model_class(cfg.model).to(device)
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr=cfg.lr)
+    optimizer = optim.Adam(model.parameters(), lr=cfg.train.learning_rate)
 
     train_model(model, train_loader, val_loader, criterion, optimizer, cfg.num_epochs, device)
 
