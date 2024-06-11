ee54e2178b2b011ed46995a983df53f0be028622
Minhee Lee <minigimbob@gmail.com>
2024-06-12 04:28:48 +0900

    ss

--- test.py
+++ test.py
@@ -1,41 +1,44 @@
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
+from pathlib import Path
 import hydra
-from omegaconf import DictConfig, OmegaConf
-import wandb
-import datetime
 
 import model_zoo
 from dataset import KpopDataset
-from trainer import validate  # Assuming validate function is in trainer.py
+from trainer import validate_with_accuracy  # Assuming validate_with_accuracy function is in trainer.py
 
 @hydra.main(config_path='config', config_name='packed')
-def main(cfg: DictConfig):
+def main(cfg):
     device = cfg.train.device
 
     test_dataset = KpopDataset(cfg, cfg.dict_key.test)
-    test_loader = DataLoader(test_dataset, batch_size=cfg.train.valid_batch_size, shuffle=False)
+    test_loader = DataLoader(test_dataset, batch_size=cfg.train.batch_size, shuffle=False)
+
+    best_model_files_path = Path(cfg.test.best_model_files_path)
+    best_model_config_path = best_model_files_path / 'config.yaml'
+    best_model_config = hydra.utils.instantiate(hydra.utils.load_config(best_model_config_path))
+    cfg.model.cfg.update(best_model_config)
+    best_model_path = best_model_files_path / 'best_model.pth'
 
     model_class = getattr(model_zoo, cfg.model.name)
     model = model_class(cfg.model.cfg).to(device)
     criterion = nn.CrossEntropyLoss()
 
     # Load the best model
-    model.load_state_dict(torch.load('best_model.pth'))
+    model.load_state_dict(torch.load(best_model_path))
     model.eval()
 
     # Perform validation on the test set
     with torch.no_grad():
-        test_loss = validate(model, test_loader, criterion, device, scaler=None, smoothing=cfg.train.smoothing, normalize=True)
+        test_loss, label_accuracy, year_accuracy = validate_with_accuracy(
+            model, test_loader, criterion, device, smoothing=cfg.train.smoothing, normalize=True
+        )
 
     print(f'Test Loss: {test_loss:.4f}')
-    wandb.run.summary["Test Loss"] = test_loss
+    print(f'Test Label Accuracy: {label_accuracy:.4f}')
+    print(f'Test Year Accuracy: {year_accuracy:.4f}')
 
-    # Save the final model if needed
-    torch.save(model.state_dict(), 'final_model.pth')
-    wandb.save('final_model.pth')
-    wandb.finish()
 
 if __name__ == '__main__':
     main()
